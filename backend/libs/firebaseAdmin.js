999d2a000038d203fe7fbfc95a213a48ace506de
Athulrathor <athulrealme2@gmail.com>
2026-06-25 18:55:23 +0530

    firebase config error fix

--- backend/libs/firebaseAdmin.js
+++ backend/libs/firebaseAdmin.js
@@ -1,9 +1,28 @@
+// import { initializeApp, cert } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+// import serviceAccount from "../twitter-a638f-firebase-adminsdk-fbsvc-2335018a9d.json" with { type: "json" };
+
+// const firebaseApp = initializeApp({
+//   credential: cert(serviceAccount),
+// });
+
+// export const fireAuth = getAuth(firebaseApp);
+
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "../twitter-a638f-firebase-adminsdk-fbsvc-2335018a9d.json" with { type: "json" };
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing");
+}
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 const firebaseApp = initializeApp({
   credential: cert(serviceAccount),
 });
 
-export const fireAuth = getAuth(firebaseApp);
+const fireAuth = getAuth(firebaseApp);
+
+export default fireAuth;
