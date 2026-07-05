0d4534be4e9f383e94c4d1bc83b157ae87e31744
Athulrathor <athulrealme2@gmail.com>
2026-07-05 17:15:28 +0530

    fixed build error

---
+++ frontend/src/modals/VerifyEmailDialog.tsx/VerifyEmailDialog.tsx
@@ -0,0 +1,27 @@
+// "use client";
+
+// import { useAuth } from '@/context/AuthContext';
+// import axiosInstance from '@/lib/axiosInstance';
+// import React, { useEffect, useState } from 'react';
+
+// interface VerifyEmailDialogProps {
+//   open: boolean;
+//   onOpenChange: (open: boolean) => void;
+// }
+
+// const VerifyEmailDialog = ({open,onOpenChange}) => {
+
+//     const { firebaseUid,fetchSession,setUser } = useAuth();
+
+// useEffect(() => {
+//     if (open) {
+        
+//     }
+// }, [open]);
+
+//   return (
+//     <div>VerifyEmailDialog</div>
+//   )
+// }
+
+// export default VerifyEmailDialog
